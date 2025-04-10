221e93aa20e3eeac5d01b5d4766acefdc1fc943a
shuang <shenghui.huang@unito.it>
2025-04-10 21:45:55 +0000

    update log key

--- src/scanpy_mcp/util.py
+++ src/scanpy_mcp/util.py
@@ -3,10 +3,9 @@
 def add_op_log(adata, func, kwargs):
     if "operation" not in adata.uns:
         adata.uns["operation"] = {}
-        adata.uns["operation"]["running"] = []
+        adata.uns["operation"]["adata"] = []
     if hasattr(func, "__name__"):
         func_name = func.__name__
     else:
         func_name = func.func.__name__
-    adata.uns["operation"]["running"].append({func_name: kwargs})
-    
+    adata.uns["operation"]["adata"].append({func_name: kwargs})
