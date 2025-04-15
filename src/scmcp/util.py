c5bde3d626273799a0d370d2efd794213fdf2f2e
shuang <shenghui.huang@unito.it>
2025-04-15 08:05:47 +0000

    process more fig

--- src/scmcp/util.py
+++ src/scmcp/util.py
@@ -14,16 +14,23 @@ def add_op_log(adata, func, kwargs):
     adata.uns["operation"]["adata"].append({func_name: kwargs})
 
 
-def set_fig_path(func):
+def set_fig_path(func, **kwargs):
     fig_dir = Path(os.getcwd()) / "figures"
-    fig_path = fig_dir / f"{func[3:]}.png"
+
+    if func == "pl_rank_genes_groups_dotplot":
+        old_path = fig_dir / 'dotplot_.png'
+        fig_path = fig_dir / f"{func[3:]}.png"
+    elif func in ["pl_scatter", "pl_embedding"]:
+        if "basis" in kwargs and kwargs['basis'] is not None:
+            old_path = fig_dir / f"{kwargs['basis']}.png"
+            fig_path = fig_dir / f"{func[3:]}_{kwargs['basis']}.png"
+    else:
+        old_path = fig_dir / f"{func[3:]}_.png"
+        fig_path = fig_dir / f"{func[3:]}.png"        
     try:
-        if func == "pl_rank_genes_groups_dotplot":
-            os.rename(fig_dir/'dotplot_.png', fig_path)
-        else:
-            os.rename(fig_dir/f'{func[3:]}_.png', fig_path)
+        os.rename(old_path, fig_path)
     except FileNotFoundError:
-        print(f"The file {fig_dir/f'{func[3:]}_.png'} does not exist")
+        print(f"The file {old_path} does not exist")
     except FileExistsError:
         print(f"The file {fig_path} already exists")
     except PermissionError:
@@ -34,7 +41,7 @@ def set_fig_path(func):
     else:
         host = os.environ.get("SCMCP_HOST")
         port = os.environ.get("SCMCP_PORT")
-        fig_path = f"http://{host}:{port}/figures/{func[3:]}.png"
+        fig_path = f"http://{host}:{port}/figures/{Path(fig_path).name}"
         return fig_path
 
 
